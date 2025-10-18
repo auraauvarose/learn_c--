f7edb99f21a38968c52c4e77705151604be55cff
auraauvarosee <auraauvaroseendica@gmail.com>
2025-10-18 23:00:43 +0700

    upload and update

--- C++ BAB KULIAH/BAB ll/operasi_percabangan.cpp
+++ C++ BAB KULIAH/BAB ll/operasi_percabangan.cpp
@@ -38,10 +38,47 @@ void lebih_dari_dua_kondisi() {
     }
 }
 
+// pernyataan if bersarang
+void if_bersarang() {
+    int nilai = 75;
+    bool hadir = true;
+
+    if (hadir) {
+        if (nilai >= 60) {
+            std::cout << " Anda Lulus " << std::endl;
+        } else {
+            std::cout << " Anda Tidak Lulus " << std::endl;
+        }
+    } else {
+        std::cout << " Anda Tidak Bisa Mengikuti Ujian Karena Tidak Hadir " << std::endl;
+    }
+}
+
+// pernyataan switch case
+void contoh_switch_case() {
+    int hari = 3; // 1=Senin, 2=Selasa, dst.
+
+    switch (hari) {
+        case 1:
+            std::cout << "Hari ini Senin." << std::endl;
+            break; // 'break' penting agar tidak lanjut ke case berikutnya
+        case 2:
+            std::cout << "Hari ini Selasa." << std::endl;
+            break;
+        case 3:
+            std::cout << "Hari ini Rabu." << std::endl; // Ini yang akan jalan
+            break;
+        default: // Mirip seperti 'else' terakhir
+            std::cout << "Nomor hari tidak valid." << std::endl;
+            break;
+    }
+}
 
 int main() {
     contoh_if();
     dua_kondisi();
     lebih_dari_dua_kondisi();
+    if_bersarang();
+    contoh_switch_case();
     return 0;
 }
