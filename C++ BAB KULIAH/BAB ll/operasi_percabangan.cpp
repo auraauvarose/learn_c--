a54fba78b64dd1c1559cb2ae3e80a8d997786612
auraauvarosee <auraauvaroseendica@gmail.com>
2025-10-18 22:51:40 +0700

    update if else

--- C++ BAB KULIAH/BAB ll/operasi_percabangan.cpp
+++ C++ BAB KULIAH/BAB ll/operasi_percabangan.cpp
@@ -9,7 +9,18 @@ void contoh_if() {
     }
 
     std::cout << " Program selesai " << std::endl;
-} 
+}
+
+
+void dua_kondisi() {
+    int nilai = 23;
+
+    if (nilai >= 60) {
+        std::cout << " Selamat, Anda Lulus " << std::endl;
+    } else {
+        std::cout << " Maaf, Anda Tidak Lulus " << std::endl;
+    }
+}
 
 
 int main() {
