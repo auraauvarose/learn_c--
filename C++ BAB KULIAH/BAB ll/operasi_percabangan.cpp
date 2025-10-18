f8fda8169675156de6daad3cee366e957f6f263f
auraauvarosee <auraauvaroseendica@gmail.com>
2025-10-18 22:53:02 +0700

    update

--- C++ BAB KULIAH/BAB ll/operasi_percabangan.cpp
+++ C++ BAB KULIAH/BAB ll/operasi_percabangan.cpp
@@ -12,6 +12,7 @@ void contoh_if() {
 }
 
 
+// if ... else (jika ... jika tidak)
 void dua_kondisi() {
     int nilai = 23;
 
@@ -22,9 +23,25 @@ void dua_kondisi() {
     }
 }
 
+// pernyataan if lebih dari dua kondisi
+void lebih_dari_dua_kondisi() {
+    int nilai = 85;
+
+    if (nilai >= 80) {
+        std::cout << " Selamat, Anda Mendapatkan Nilai A " << std::endl;
+    } else if (nilai >= 70) {
+        std::cout << " Anda Mendapatkan Nilai B " << std::endl;
+    } else if (nilai >= 60) {
+        std::cout << " Anda Mendapatkan Nilai C " << std::endl;
+    } else {
+        std::cout << " Maaf, Anda Tidak Lulus " << std::endl;
+    }
+}
+
 
 int main() {
     contoh_if();
     dua_kondisi();
+    lebih_dari_dua_kondisi();
     return 0;
 }
