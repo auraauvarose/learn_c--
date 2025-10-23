9346d99e47af120283516db89b10fcb910c00915
auraauvarosee <auraauvaroseendica@gmail.com>
2025-10-23 19:45:36 +0700

    selesai

--- C++ BAB KULIAH/BAB IV/latihan_tugas_bab_4.cpp
+++ C++ BAB KULIAH/BAB IV/latihan_tugas_bab_4.cpp
@@ -4,30 +4,20 @@
 using namespace std;
 
 int main() {
-    // --- Deklarasi Variabel ---
     string nama;
     string status_juara;
     double nilai1, nilai2, rata_rata;
 
-    // --- Layar Masukkan ---
     cout << "PROGRAM HITUNG NILAI RATA-RATA" << endl;
     cout << "-----------------------------" << endl;
-
-    // Menggunakan getline untuk membaca nama yang mungkin mengandung spasi
     cout << "Nama Peserta : ";
     getline(cin, nama);
-
     cout << "Nilai I : ";
     cin >> nilai1;
-
     cout << "Nilai II : ";
     cin >> nilai2;
-
-    // --- Proses Perhitungan dan Penentuan Juara ---
-    // Menghitung nilai rata-rata
     rata_rata = (nilai1 + nilai2) / 2;
 
-    // Menentukan status juara berdasarkan nilai rata-rata
     if (rata_rata > 80) {
         status_juara = "Juara Umum";
     } else if (rata_rata > 75) {
@@ -38,7 +28,6 @@ int main() {
         status_juara = "tidak juara";
     }
 
-    // --- Layar Keluaran ---
     cout << "\n--- Hasil ---" << endl;
     cout << "Siswa yang bernama " << nama 
          << " memperoleh nilai rata-rata " << rata_rata 
