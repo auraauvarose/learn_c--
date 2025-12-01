19a575da7c709e57befb93d812ed80033692ecac
auraauvarosee <auraauvaroseendica@gmail.com>
2025-12-01 22:48:00 +0700

    update tugas

---
+++ Flowchart Tugas/pengolahan_data_nilai_siswa.cpp
@@ -0,0 +1,33 @@
+/*
+TUGAS untuk minggu depan :
+1. Buat Judul Aplikasi (contoh : "aplikasi Pengolahan data nilai siswa")
+2. Buat deskripsi Aplikasi (contoh : "a. Masukkan nama siswa , b. Masukkan jumlah pelajaran , c. Masukkan nama pelajaran dan nilai (diulang sebanyak jumlah pelajaran yang diinputkan), d. hitung rata-rata nilai, e. tampilkan nama siswa, rata-rata nilai, dan kesimpulan lulus atau tidak, f. selesai)
+3. Buatkan Flowchart untuk deskripsi tersebut
+
+tugas boleh di tulis tangan atau di ketik 
+minggu depan di konsultasikan
+*/
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    // deklarasi variabel
+    double total = 0;
+    int i = 0;
+    const int KRITERIA_LULUS = 75;
+
+    // variabel input
+    string namasiswa;
+    int jumlah;
+    const int MAX = 100;
+
+
+}
+
+
+
