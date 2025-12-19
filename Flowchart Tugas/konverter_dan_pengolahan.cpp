86fb89d412197493addb8ba50a767e90308949f9
auraauvarosee <auraauvaroseendica@gmail.com>
2025-12-19 19:44:14 +0700

    update

---
+++ Flowchart Tugas/konverter_dan_pengolahan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+void konverterMataUang() {
+    const double Tukar_USD = 16604.0;
+    const double Tukar_EUR = 19303.0;
+    const double Tukar_JPY = 106.0;
+
+
+    
+}
+
+
+
+
+
